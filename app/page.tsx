667f4139cb971c5982bc883c103552c144a63bdf
yamdev07 <yamdyamd080@gmail.com>
2026-07-03 15:19:31 +0100

    feat: section a propos et statistiques animees de l'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/Hero";
 import Services from "@/components/home/Services";
+import About from "@/components/home/About";
 
 export default function Home() {
   return (
     <main id="main">
       <Hero />
       <Services />
+      <About />
     </main>
   );
 }
